51918d93b20243cadea9e05f876c6c8457e8fc1e
Rino-9 <124519099+Rino-9@users.noreply.github.com>
2023-02-09 08:18:48 +0300

    Tovar

---
+++ exercise1/html.php
@@ -0,0 +1,15 @@
+<html>
+<form method="POST" name="Tovar" action="/html.php">
+    <input type="text" name="milk2"><br><br>
+       <input type="text" name="milk"><br><br>
+      <input type="submit" name="inaform" value="Отправить">
+
+
+
+
+
+</form>
+
+
+
+</html>
